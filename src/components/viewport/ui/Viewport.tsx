4a9fdb6be731168414b49ab7f214edcf564ba83d
rodertd2000 <zdabaghov@mail.ru>
2024-09-02 17:12:24 +0300

    spinner

--- src/components/viewport/ui/Viewport.tsx
+++ src/components/viewport/ui/Viewport.tsx
@@ -2,8 +2,12 @@ import { Button } from "@/components/ui/button";
 import { Drawer, DrawerTrigger, DrawerContent } from "@/components/ui/drawer";
 import { useEditor } from "@craftjs/core";
 import { parseStructure, parseStructureToString } from "../../../lib/parse";
+import { useState } from "react";
+import { Spinner } from "@/components/spinner";
 
 export const Viewport = ({ children }: { children: React.ReactNode }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const { query } = useEditor((state) => ({
     query: state.nodes,
   }));
@@ -11,47 +15,58 @@ export const Viewport = ({ children }: { children: React.ReactNode }) => {
   // @ts-ignore
   const state = query.getSerializedNodes();
   const handleExport = async () => {
-    const { componentCode, components } = parseStructureToString(state);
+    setIsLoading(true);
 
-    const response = await fetch("/api/export", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ componentCode, components }),
-    });
+    try {
+      const { componentCode, components } = parseStructureToString(state);
 
-    const blob = await response.blob();
+      const response = await fetch("/api/export", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ componentCode, components }),
+      });
 
-    // Create a link element to download the Blob
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = `$export.zip`;
-    document.body.appendChild(a);
-    a.click();
+      const blob = await response.blob();
 
-    // Clean up by revoking the object URL and removing the element
-    setTimeout(() => {
-      window.URL.revokeObjectURL(url);
-      document.body.removeChild(a);
-    }, 0);
+      // Create a link element to download the Blob
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = `$export.zip`;
+      document.body.appendChild(a);
+      a.click();
+
+      // Clean up by revoking the object URL and removing the element
+      setTimeout(() => {
+        window.URL.revokeObjectURL(url);
+        document.body.removeChild(a);
+      }, 0);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const { renderComponent } = parseStructure(state);
 
   return (
     <div className="viewport w-full overflow-y-auto overflow-x-hidden">
-      <Drawer>
-        <DrawerTrigger asChild>
-          <Button variant="outline">Open Drawer</Button>
-        </DrawerTrigger>
-        <DrawerContent className="w-full h-[80%] p-2 m-2">
-          {<div>{renderComponent}</div>}
-        </DrawerContent>
-      </Drawer>
+      <div className="flex justify-between p-4 pb-0">
+        <Drawer>
+          <DrawerTrigger asChild>
+            <Button variant="outline">Open Drawer</Button>
+          </DrawerTrigger>
+          <DrawerContent className="w-full h-[80%] p-2 m-2">
+            {<div>{renderComponent}</div>}
+          </DrawerContent>
+        </Drawer>
 
-      <button onClick={handleExport}>Export</button>
+        <Button onClick={handleExport}>Export</Button>
+      </div>{" "}
+      {isLoading && <Spinner />}
       <div className={"craftjs-renderer flex-1 h-full w-full"}>{children}</div>
     </div>
   );
