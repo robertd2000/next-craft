2efbf7f9fc2509f836293bb1992d4ef5492585a0
rodertd2000 <zdabaghov@mail.ru>
2024-09-04 16:08:56 +0300

    Accordion

--- src/components/controls/ui/SettingsControl.tsx
+++ src/components/controls/ui/SettingsControl.tsx
@@ -184,6 +184,7 @@ export function SettingsControl({ children }: SettingsControlProps) {
             "Position",
             "Typography",
             "Backgrounds",
+            "Borders",
           ]}
         >
           <Display />
