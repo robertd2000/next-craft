9a3321f043695ce6dc9181ae97d04d824d1b63a5
rodertd2000 <zdabaghov@mail.ru>
2024-09-04 10:28:51 +0300

    Size

--- src/components/controls/ui/size/ui/Size.tsx
+++ src/components/controls/ui/size/ui/Size.tsx
@@ -1,6 +1,6 @@
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { useNode } from "@craftjs/core";
+import { Label } from "@/components/ui/label";
+import { InputMeasure } from "../../input-measure";
 
 export function Size() {
   const {
@@ -17,95 +17,65 @@ export function Size() {
       <div className="grid grid-cols-2 gap-2 mt-4 mb-4">
         <div>
           <Label htmlFor="width">Width</Label>
-          <Input
+          <InputMeasure
             id="width"
             value={props.style?.width}
-            onChange={(e) => {
-              setProp(
-                (props: { style: { width: string } }) =>
-                  (props.style = { ...props.style, width: e.target.value }),
-                500
-              );
-            }}
+            setProp={setProp}
+            propName="width"
           />
         </div>
 
         <div>
           <Label htmlFor="height">Height</Label>
-          <Input
+          <InputMeasure
             id="height"
             value={props.style?.height}
-            onChange={(e) => {
-              setProp(
-                (props: { style: { height: string } }) =>
-                  (props.style = { ...props.style, height: e.target.value }),
-                500
-              );
-            }}
+            setProp={setProp}
+            propName="height"
           />
         </div>
       </div>
 
       <div className="grid grid-cols-2 gap-2 mt-4 mb-4">
         <div>
-          <Label htmlFor="min-width">Min Width</Label>
-          <Input
-            id="min-width"
+          <Label htmlFor="minWidth">Min Width</Label>
+          <InputMeasure
+            id="minWidth"
             value={props.style?.minWidth}
-            onChange={(e) => {
-              setProp(
-                (props: { style: { minWidth: string } }) =>
-                  (props.style = { ...props.style, minWidth: e.target.value }),
-                500
-              );
-            }}
+            setProp={setProp}
+            propName="minWidth"
           />
         </div>
 
         <div>
-          <Label htmlFor="min-height">Min Height</Label>
-          <Input
-            id="min-height"
+          <Label htmlFor="minHeight">Min Height</Label>
+          <InputMeasure
+            id="minHeight"
             value={props.style?.minHeight}
-            onChange={(e) => {
-              setProp(
-                (props: { style: { minHeight: string } }) =>
-                  (props.style = { ...props.style, minHeight: e.target.value }),
-                500
-              );
-            }}
+            setProp={setProp}
+            propName="minHeight"
           />
         </div>
       </div>
 
       <div className="grid grid-cols-2 gap-2 mt-4 mb-4">
         <div>
-          <Label htmlFor="max-width">Max Width</Label>
-          <Input
-            id="max-width"
+          <Label htmlFor="maxWidth">Max Width</Label>
+          <InputMeasure
+            id="maxWidth"
             value={props.style?.maxWidth}
-            onChange={(e) => {
-              setProp(
-                (props: { style: { maxWidth: string } }) =>
-                  (props.style = { ...props.style, maxWidth: e.target.value }),
-                500
-              );
-            }}
+            setProp={setProp}
+            propName="maxWidth"
           />
         </div>
 
         <div>
-          <Label htmlFor="max-height">Max Height</Label>
-          <Input
-            id="max-height"
+          <Label htmlFor="maxHeight">Max Height</Label>
+          <InputMeasure
+            id="maxHeight"
             value={props.style?.maxHeight}
-            onChange={(e) => {
-              setProp(
-                (props: { style: { maxHeight: string } }) =>
-                  (props.style = { ...props.style, maxHeight: e.target.value }),
-                500
-              );
-            }}
+            setProp={setProp}
+            propName="maxHeight"
           />
         </div>
       </div>
