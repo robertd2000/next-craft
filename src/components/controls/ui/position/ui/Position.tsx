4ad07bcd6d6a2ba2249ce41bec2c5c9035b2df2a
rodertd2000 <zdabaghov@mail.ru>
2024-09-04 10:58:42 +0300

    Position

--- src/components/controls/ui/position/ui/Position.tsx
+++ src/components/controls/ui/position/ui/Position.tsx
@@ -9,6 +9,11 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export function Position() {
   const {
@@ -19,41 +24,43 @@ export function Position() {
   }));
 
   return (
-    <div className="border-b border-b-1 mt-2">
-      <h2 className="font-bold">Position</h2>
+    <AccordionItem value="Position">
+      <AccordionTrigger>Position</AccordionTrigger>
 
-      <div className="flex gap-2 items-center mt-2 mb-2">
-        <Label htmlFor="position">Position</Label>
-        <Select
-          value={props.style?.position}
-          defaultValue="static"
-          onValueChange={(e) => {
-            setProp(
-              (props: { style: { position: string } }) =>
-                (props.style = { ...props.style, position: e }),
-              500
-            );
-          }}
-        >
-          <SelectTrigger className="w-full" id="position">
-            <SelectValue placeholder="Select a position" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectLabel>Position</SelectLabel>
-              <SelectItem value="static">static</SelectItem>
-              <SelectItem value="relative">relative</SelectItem>
-              <SelectItem value="absolute">absolute</SelectItem>
-              <SelectItem value="sticky">sticky</SelectItem>
-              <SelectItem value="fixed">fixed</SelectItem>
-              <SelectItem value="inherit">inherit</SelectItem>
-              <SelectItem value="revert">revert</SelectItem>
-              <SelectItem value="revert-layer">revert-layer</SelectItem>
-              <SelectItem value="unset">unset</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </div>
-    </div>
+      <AccordionContent>
+        <div className="flex gap-2 items-center mt-2 mb-2">
+          <Label htmlFor="position">Position</Label>
+          <Select
+            value={props.style?.position}
+            defaultValue="static"
+            onValueChange={(e) => {
+              setProp(
+                (props: { style: { position: string } }) =>
+                  (props.style = { ...props.style, position: e }),
+                500
+              );
+            }}
+          >
+            <SelectTrigger className="w-full" id="position">
+              <SelectValue placeholder="Select a position" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Position</SelectLabel>
+                <SelectItem value="static">static</SelectItem>
+                <SelectItem value="relative">relative</SelectItem>
+                <SelectItem value="absolute">absolute</SelectItem>
+                <SelectItem value="sticky">sticky</SelectItem>
+                <SelectItem value="fixed">fixed</SelectItem>
+                <SelectItem value="inherit">inherit</SelectItem>
+                <SelectItem value="revert">revert</SelectItem>
+                <SelectItem value="revert-layer">revert-layer</SelectItem>
+                <SelectItem value="unset">unset</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+      </AccordionContent>
+    </AccordionItem>
   );
 }
