b06cd0abca51145e7f6299de7200b9615e279586
rodertd2000 <zdabaghov@mail.ru>
2024-09-04 10:57:47 +0300

    Size

--- src/components/controls/ui/size/ui/Size.tsx
+++ src/components/controls/ui/size/ui/Size.tsx
@@ -1,6 +1,11 @@
 import { useNode } from "@craftjs/core";
 import { Label } from "@/components/ui/label";
 import { InputMeasure } from "../../input-measure";
+import {
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export function Size() {
   const {
@@ -11,74 +16,76 @@ export function Size() {
   }));
 
   return (
-    <div className="border-b border-b-1 mt-2">
-      <h2 className="font-bold">Size</h2>
+    <AccordionItem value="Size">
+      <AccordionTrigger>Size</AccordionTrigger>
 
-      <div className="grid grid-cols-2 gap-2 mt-4 mb-4">
-        <div>
-          <Label htmlFor="width">Width</Label>
-          <InputMeasure
-            id="width"
-            value={props.style?.width}
-            setProp={setProp}
-            propName="width"
-          />
+      <AccordionContent>
+        <div className="grid grid-cols-2 gap-2 mt-4 mb-4">
+          <div>
+            <Label htmlFor="width">Width</Label>
+            <InputMeasure
+              id="width"
+              value={props.style?.width}
+              setProp={setProp}
+              propName="width"
+            />
+          </div>
+
+          <div>
+            <Label htmlFor="height">Height</Label>
+            <InputMeasure
+              id="height"
+              value={props.style?.height}
+              setProp={setProp}
+              propName="height"
+            />
+          </div>
         </div>
 
-        <div>
-          <Label htmlFor="height">Height</Label>
-          <InputMeasure
-            id="height"
-            value={props.style?.height}
-            setProp={setProp}
-            propName="height"
-          />
-        </div>
-      </div>
+        <div className="grid grid-cols-2 gap-2 mt-4 mb-4">
+          <div>
+            <Label htmlFor="minWidth">Min Width</Label>
+            <InputMeasure
+              id="minWidth"
+              value={props.style?.minWidth}
+              setProp={setProp}
+              propName="minWidth"
+            />
+          </div>
 
-      <div className="grid grid-cols-2 gap-2 mt-4 mb-4">
-        <div>
-          <Label htmlFor="minWidth">Min Width</Label>
-          <InputMeasure
-            id="minWidth"
-            value={props.style?.minWidth}
-            setProp={setProp}
-            propName="minWidth"
-          />
-        </div>
-
-        <div>
-          <Label htmlFor="minHeight">Min Height</Label>
-          <InputMeasure
-            id="minHeight"
-            value={props.style?.minHeight}
-            setProp={setProp}
-            propName="minHeight"
-          />
-        </div>
-      </div>
-
-      <div className="grid grid-cols-2 gap-2 mt-4 mb-4">
-        <div>
-          <Label htmlFor="maxWidth">Max Width</Label>
-          <InputMeasure
-            id="maxWidth"
-            value={props.style?.maxWidth}
-            setProp={setProp}
-            propName="maxWidth"
-          />
+          <div>
+            <Label htmlFor="minHeight">Min Height</Label>
+            <InputMeasure
+              id="minHeight"
+              value={props.style?.minHeight}
+              setProp={setProp}
+              propName="minHeight"
+            />
+          </div>
         </div>
 
-        <div>
-          <Label htmlFor="maxHeight">Max Height</Label>
-          <InputMeasure
-            id="maxHeight"
-            value={props.style?.maxHeight}
-            setProp={setProp}
-            propName="maxHeight"
-          />
+        <div className="grid grid-cols-2 gap-2 mt-4 mb-4">
+          <div>
+            <Label htmlFor="maxWidth">Max Width</Label>
+            <InputMeasure
+              id="maxWidth"
+              value={props.style?.maxWidth}
+              setProp={setProp}
+              propName="maxWidth"
+            />
+          </div>
+
+          <div>
+            <Label htmlFor="maxHeight">Max Height</Label>
+            <InputMeasure
+              id="maxHeight"
+              value={props.style?.maxHeight}
+              setProp={setProp}
+              propName="maxHeight"
+            />
+          </div>
         </div>
-      </div>
-    </div>
+      </AccordionContent>
+    </AccordionItem>
   );
 }
