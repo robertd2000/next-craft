7cf28d3a89c87e44208163b3fa5574a21b81b01f
rodertd2000 <zdabaghov@mail.ru>
2024-09-04 16:17:08 +0300

    borderColor

--- src/components/controls/ui/borders/ui/Borders.tsx
+++ src/components/controls/ui/borders/ui/Borders.tsx
@@ -7,6 +7,7 @@ import { Label } from "@/components/ui/label";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useNode } from "@craftjs/core";
 import { X, Minus, Ellipsis, ChevronsLeftRightEllipsis } from "lucide-react";
+import { HexColorPicker } from "react-colorful";
 
 export function Borders() {
   const {
@@ -53,6 +54,21 @@ export function Borders() {
               </TabsTrigger>
             </TabsList>
           </Tabs>
+        </div>
+
+        <div className="flex flex-wrap justify-between items-center mt-2">
+          <Label htmlFor="borderColor">Color</Label>
+          <HexColorPicker
+            id="borderColor"
+            color={props.style?.borderColor}
+            onChange={(color) => {
+              setProp(
+                (props: { style: { borderColor: string } }) =>
+                  (props.style = { ...props.style, borderColor: color }),
+                500
+              );
+            }}
+          />
         </div>
       </AccordionContent>
     </AccordionItem>
